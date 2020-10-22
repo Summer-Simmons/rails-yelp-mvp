8356caf4db8e3c112a01353483ab050c3f692985
Summer S <summer.s2020@icloud.com>
2020-10-22 23:17:41 +0900

    mostly done, except for invalid review handling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new # needed to instantiate the form_for
+    @review.restaurant = @restaurant
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,11 +2,7 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
   validates :rating, inclusion: { in: (0..5) }
   validates :content, presence: true
-
-  # before_create do |review|
-  #   p review.rating
-  #   review.rating = -1 if review.rating.is_a? Integer == false
-  # end
 end
